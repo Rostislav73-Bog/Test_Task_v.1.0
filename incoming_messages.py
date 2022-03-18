2f4fde27ce02a8411abedd41e3e047d43099b6d7
Rostislav73-Bog <rostislav-bogdanov@mail.ru>
2022-03-18 18:26:12 +0700

    add addiction

---
+++ incoming_messages.py
@@ -0,0 +1,46 @@
+from fastapi import APIRouter, Request
+
+from messages import Message
+
+router = APIRouter(
+    prefix="/messages",
+    tags=["messages"]
+)
+
+data = []
+
+message = Message(
+    id=1,
+    text="kdvnkdokfebe",
+)
+
+data.append(message)
+
+
+@router.get("/{id}")
+async def sms(id: int):
+    for message in data:
+        if message.id == id:
+            return message
+
+
+@router.post("/{id}")
+async def add_text(message: Message):
+    message.id = data[-1].id + 1
+    data.append(message)
+    return data
+
+
+@router.put("/{id}")
+async def update_text(id: int, message: Message):
+    for all_message in data:
+        if all_message.id == id:
+            all_message.text = message.text
+            return all_message
+
+
+@router.delete("/{id}")
+async def del_text(id: int):
+    for message in data:
+        if message.id == id:
+            data.remove(message)
---
+++ main.py
@@ -0,0 +1,63 @@
+from fastapi import FastAPI, WebSocket, Request
+from fastapi.responses import HTMLResponse
+from fastapi.responses import Response
+from incoming_messages import data
+from incoming_messages import router
+
+
+app = FastAPI()
+
+app.include_router(router)
+
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Message</title>
+    </head>
+    <body style="color:#2F4F4F; background-color:#FFF0F5">
+        <h1>Messages</h1>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off"/>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+            var ws = new WebSocket("ws://localhost:8000/ws");
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
+
+
+@app.get("/")
+async def get():
+    return HTMLResponse(html)
+
+
+@app.websocket("/ws")
+@app.get("/")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    index = 0
+    while True:
+        index += 1
+        data = await websocket.receive_text()
+        print(index, ".", data)
+        await websocket.send_text(f"{index}. {data}")
+
+
