991c1a5e5c999220bf358558aa820d8d82aefa05
Rostislav73-Bog <rostislav-bogdanov@mail.ru>
2022-03-25 14:35:18 +0700

    Changed JSON(obj)

--- main.py
+++ main.py
@@ -1,61 +1,30 @@
-import starlette
 from fastapi import FastAPI, WebSocket
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, FileResponse
 from incoming_messages import router
 
 app = FastAPI()
 
 app.include_router(router)
 
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-
-        <title>Message</title>
-    </head>
-    <body style="color:#2F4F4F; background-color:#FFF0F5">
-        <h1>Messages</h1>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="JSON.stringify" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
-            var ws = new WebSocket("ws://localhost:8000/ws");
-            ws.onmessage = function() {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage() {
-                var input = document.getElementById("messageText")
-                ws.send(JSON.stringify(input.value))
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
-    </body>
-</html>
-"""
 
 
-@app.get("/")
+
+@app.get("/", response_class=HTMLResponse)
 async def get():
-    return HTMLResponse(html)
+    return FileResponse("main.html")
 
 
 @app.websocket("/ws")
 @app.get("/")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
-    index = 0
     while True:
-        index += 1
         data = await websocket.receive_json()
-        print(index, ".", data)
-        await websocket.send_json(f"{index}. {data}")
+        request_message = {
+
+            'id': data['messageId'],
+            'body': data['body']
+
+        }
+        await websocket.send_json(request_message)
 
